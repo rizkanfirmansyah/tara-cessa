e48e0b8e121c619ed541509a7590bffea866d6db
rizkanxcode <riezkanaprianda@gmail.com>
2024-08-29 12:17:38 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["9.9.9.2", "34.101.176.12", "api.siorta.com", "34.50.70.145", "319d-180-254-226-123.ngrok-free.app"],
+        domains: ["9.9.9.2", "34.101.176.12", "api.siorta.com", "34.50.70.145", "319d-180-254-226-123.ngrok-free.app", "thara-api.chesacanggu.com", "thara-dashboard.chesacanggu.com", "43.248.27.12:8100", "43.248.27.12:81"],
     },
 };
 
